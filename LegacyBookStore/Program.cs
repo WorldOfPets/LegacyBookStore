c4a5bc1f033c8d94fba500c2ef037772adc25f22
Andrei <chernenkoa04@gmail.com>
2025-09-30 11:32:49 +0700

    added cors

--- LegacyBookStore/Program.cs
+++ LegacyBookStore/Program.cs
@@ -3,12 +3,23 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 var app = builder.Build();
 
+app.UseCors();
 
 app.Use(async (context, next) =>
 {
